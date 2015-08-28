b75a333ade79431acfe522bb2d285903615d2be4
seisou01 <mo.shimokura@city.matsusaka.mie.jp>
2015-08-28 16:20:38 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -5,7 +5,7 @@
 
 var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+var MaxDescription = 10; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 3;
 
